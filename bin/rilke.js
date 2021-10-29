70fb9800982acae15deac919e8dca4a1612d7537
Emirhan Yumak <emiryumak@gmail.com>
2021-10-29 13:34:26 +0300

    CLI Bin

---
+++ bin/rilke.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+var shell = require("shelljs");
+
+shell.exec("ng new project --collection @kantist/rilke-cli");
+
+// module.exports = function () {
+// 	var header = process.argv[2];
+// }
+
