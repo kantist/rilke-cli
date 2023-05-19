c094f07aa61a08ed31c1d7f9e41eb1d9752234b5
emiryumak <emiryumak@gmail.com>
2023-05-19 13:32:52 +0300

    Remove Spec file

--- src/dockerfile/index_spec.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright Kant Yazılım A.Ş. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://rilke.ist/license
- */
-export {};
